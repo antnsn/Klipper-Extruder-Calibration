c5abd5e5bd13ed648eb4d902fd7d09b7667d6bba
Marius Antonsen <antonsen.marius@gmail.com>
2023-10-02 16:53:31 +0200

    Adder webui and fixed calculations to better suit klipper
    Signed-off-by: Marius Antonsen <antonsen.marius@gmail.com>

--- app.py
+++ app.py
@@ -4,20 +4,22 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    result = None
     if request.method == 'POST':
-        try:
-            motor_steps = float(request.form['motor_steps'])
-            microstepping = float(request.form['microstepping'])
-            belt_pitch = float(request.form['belt_pitch'])
-            pulley_tooth = float(request.form['pulley_tooth'])
-            
-            # Calculate steps per mm
-            result = (motor_steps * microstepping) / (belt_pitch * pulley_tooth)
-        except ValueError:
-            result = "Invalid Input"
+        # Get input values
+        prev_rotation_distance = float(request.form.get('prev_rotation_distance'))
+        initial_mark_distance = float(request.form.get('initial_mark_distance'))
+        subsequent_mark_distance = float(request.form.get('subsequent_mark_distance'))
+        requested_extrude = float(request.form.get('requested_extrude'))
 
-    return render_template('index.html', result=result)
+        # Calculate the actual extrude distance
+        actual_extrude_distance = initial_mark_distance - subsequent_mark_distance
+
+        # Calculate the new rotation_distance
+        new_rotation_distance = round(prev_rotation_distance * actual_extrude_distance / requested_extrude, 3)
+
+        return render_template('index.html', result=new_rotation_distance)
+    else:
+        return render_template('index.html', result=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ dockerfile
@@ -0,0 +1,20 @@
+# Use the official Python image from the DockerHub
+FROM python:3.9-slim
+
+# Set the working directory inside the container
+WORKDIR /app
+
+# Copy the dependencies file to the working directory
+COPY requirements.txt .
+
+# Install dependencies
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy the content of the local src directory to the working directory
+COPY . .
+
+EXPOSE 5000
+
+# Specify the command to run on container start
+CMD ["python", "./app.py"]
+
